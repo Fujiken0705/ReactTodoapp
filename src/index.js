cd59d86959438db8354600c91064f464fcf75359
Fujiken0705 <kentofujita.new@gmail.com>
2022-10-04 22:44:25 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//getbyElementはindex.htmlに相当？だからdivの中が空でもAppが表示される。
 root.render(
   <React.StrictMode>
     <App />
